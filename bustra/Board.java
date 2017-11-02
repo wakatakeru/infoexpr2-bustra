6fe458e2340380f4b95aa92cbd1672ce7e42da16
wakatakeru <s15t233@stu.kagawa-u.ac.jp>
2017-11-02 21:05:57 +0900

    コンストラクタを定義

--- bustra/Board.java
+++ bustra/Board.java
@@ -1,40 +1,60 @@
+package bustra;
+
 public class Board {
   private int size;
   private int eraseBlocksCount = 0;
   private int erasingCount = 0;
-  private Block[][] blocks;
+  // private Block[][] blocks;
  
-  public Board() {
-    
+  public Board(int boardSize) {
+    this.size = boardSize;
+    setBoard(this.size);
   }
 
   public static boolean moveBlock(int hand) {
-    
+    return true; 
   }
 
   // detectLineを定義する
   
   public static boolean setBoard(int size) {
-    
+
+    // 動作テスト
+    return true;
   }
 
   public static int getErasedBlocksCount() {
-    
+
+    // 動作テスト
+    return 0;
   }
 
   public static int getErasingCount() {
     
+    // 動作テスト
+    return 0;
   }
 
   private static boolean  eraseBlocks() {
-    
+
+    // 動作テスト
+    return true;
   }
   
   private static boolean slideBlocks() {
-    
+
+    // 動作テスト
+    return true;
   }
   
   private static boolean appendBlocks() {
-    
-  }  
+
+    // 動作テスト
+    return true;
+  }
+
+  // デバッグ用メイン関数
+  public static void main(String args[]) {
+    Board board = new Board(10);
+  }
 }
