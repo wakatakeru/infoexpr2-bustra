d6cfa1d69b2336bee0beb40ba2fb77ca076b44a7
wakatakeru <s15t233@stu.kagawa-u.ac.jp>
2017-11-02 20:49:00 +0900

    スケルトン構造を策定

---
+++ bustra/Board.java
@@ -0,0 +1,40 @@
+public class Board {
+  private int size;
+  private int eraseBlocksCount = 0;
+  private int erasingCount = 0;
+  private Block[][] blocks;
+ 
+  public Board() {
+    
+  }
+
+  public static boolean moveBlock(int hand) {
+    
+  }
+
+  // detectLineを定義する
+  
+  public static boolean setBoard(int size) {
+    
+  }
+
+  public static int getErasedBlocksCount() {
+    
+  }
+
+  public static int getErasingCount() {
+    
+  }
+
+  private static boolean  eraseBlocks() {
+    
+  }
+  
+  private static boolean slideBlocks() {
+    
+  }
+  
+  private static boolean appendBlocks() {
+    
+  }  
+}
