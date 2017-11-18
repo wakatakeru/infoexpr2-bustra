50baf2a68b1c6727d5aa5c5a4b6bb941ac7491d7
wakatakeru <s15t233@stu.kagawa-u.ac.jp>
2017-11-18 19:48:12 +0900

    JFrameで表示するように変更

--- bustra/StartMenu.java
+++ bustra/StartMenu.java
@@ -18,88 +18,81 @@ import javax.swing.JButton;
 import javax.swing.JSeparator;
 import javax.swing.SwingUtilities;
 
-public class StartMenu extends Menu implements Runnable {
+public class StartMenu extends JFrame implements Runnable {
   
   private static String userName;
-  private int width, hight;
-  private JLabel labelTitle, labelName;
-  private JTextField fieldName;
-  private JButton startButton;
+  private static int width, height;
+  private static JLabel labelTitle, labelName;
+  private static JTextField fieldName;
+  private static JButton startButton;
+  private static JPanel p;
+  private Container contentPane;
+  private static boolean inputFlag;
   
   public StartMenu() {
+    p = new JPanel();
+    contentPane = getContentPane();  
+
+    inputFlag = false;
     
     width = 300;
-    hight = 200;
-    setPreferredSize(new Dimension(width, hight));
-    setFocusable(true);
-    this.disp();
+    height = 200;
+
+    setTitle("StartMenu");
+    setSize(width, height);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLayout(new FlowLayout());
     
-  }
-  
-  // userName 返却
-  public static String getUserName() {
-    
-    return userName;
-  
-  }
-  
-  @Override
-  public boolean disp() {
-    
-    // action
+    p.setPreferredSize(new Dimension(width, height));
+    p.setFocusable(true);
+
+
     fieldName = new JTextField("", 10);
     startButton = new JButton("ゲームスタート");
+
     startButton.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
         userName = fieldName.getText();
+        inputFlag = true;
       }
     });
+
     labelTitle = new JLabel("Bustra!");
     labelTitle.setFont(new Font("Arial", Font.PLAIN, 24));
     labelName = new JLabel("Your Name");
+
+    p.add(labelTitle);
+    p.add(getLine(width, 0));
+    p.add(labelName);
+    p.add(fieldName);
+    p.add(getLine(width, 0));
+    p.add(getLine(width - 18, 1));
+    p.add(startButton);
+    p.add(getLine(width, 0));
     
-    // add and layout
-    setLayout(new FlowLayout());
-    this.add(labelTitle);
-    this.add(getLine(width, 0));
-    this.add(labelName);
-    this.add(fieldName);
-    this.add(getLine(width, 0));
-    this.add(getLine(width - 18, 1));
-    this.add(startButton);
-    this.add(getLine(width, 0));
-    
-    return true;
-    
+    contentPane.add(p);
+  }
+  
+  
+  // userName 返却
+  public static String getUserName() {
+    return userName;
+  }
+
+  public static JSeparator getLine(int wid, int hig) {
+    JSeparator sp = new JSeparator(JSeparator.HORIZONTAL);
+    sp.setPreferredSize(new Dimension(wid, hig));
+    return sp;
   }
   
   @Override
   public void run() {
-    
-    SwingUtilities.invokeLater(() -> {
-      JFrame frame = new JFrame("StartMenu");
-      
-      frame.add(new StartMenu());
-      frame.pack();
-      frame.setVisible(true);
-
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    });
-    
+    StartMenu frame = new StartMenu();
+    frame.setVisible(true);
   }
   
-  /* デバック用
   public static void main(String args[]) {
-    SwingUtilities.invokeLater(() -> {
-      JFrame frame = new JFrame("StartMenu");
-      
-      frame.add(new StartMenu());
-      frame.pack();
-      frame.setVisible(true);
-
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    });
+    StartMenu frame = new StartMenu();
+    frame.setVisible(true);
   }
-  */
-  
 }
